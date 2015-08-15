3506bd1bd3852f8c994f077af94d628bc7515c7b
Z k <awesomechaos@qq.com>
2015-08-15 12:05:34 +0800

    change dependence

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,5 @@
 /*
  * This file bootstraps the test environment.
  */
-if (!is_file(dirname(__DIR__) . '/../../vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
-
-$loader = require dirname(__DIR__) . '/../../vendor/autoload.php';
-$loader->add('Sample\\Test\\', __DIR__);
+$loader = require ( __DIR__ . '/../../../../vendor/autoload.php');
+$loader->add('Sample\\Tests\\', __DIR__);
